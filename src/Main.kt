dc79484b3b23d16ac44280ef55b45e0230d1f84c
najlaasz <najlaa.alzabi@gmail.com>
2025-02-18 19:56:06 +0300

    answers

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+
+    var  admin = UserRole.ADMIN
+    var levelNum = 2
+
+    println( "Is the level of  ${admin.name} --> $levelNum  ??? the answer is ${isSufficientLevel(admin,levelNum)} ")
+    for (user in UserRole.entries)
+    {
+        println("level: ${user.level}   name:${user.name}")
+    }
 }
